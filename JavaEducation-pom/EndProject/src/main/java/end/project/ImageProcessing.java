42ea28a7284660ecff3cdd6046ce1a7865a27fa3
Tricia@29994 <nabatanzipatricia29@gmail.com>
2022-03-07 16:32:09 +0300

    Demo of changes for Patricia

--- JavaEducation-pom/EndProject/src/main/java/end/project/ImageProcessing.java
+++ JavaEducation-pom/EndProject/src/main/java/end/project/ImageProcessing.java
@@ -1,6 +1,5 @@
 package end.project;
 
-
 import java.util.Random;
 
 public class ImageProcessing {
@@ -64,7 +63,7 @@ public class ImageProcessing {
             }
             return manipulatedImg;
         }
-
+    }
 
     public static int[][] stretchHorizontally(int[][] imageTwoD) {
         // TODO: Fill in the code for this method
@@ -179,90 +178,98 @@ public class ImageProcessing {
             int randomRowPos = rand.nextInt(canvas.length - randomHeight);
             int randomColPos = rand.nextInt(canvas[0].length - randomWidth);
             int randomColor;
-            int[] rgba = {rand.nextInt(256), rand.nextInt(256), rand.nextIntcanvas = paintRectangle(canvas, randomWidth, randomHeight, randomRowPos, randomColPos, randomColor);
+            rand.nextInt(i) canvas = paintRectangle(canvas, randomWidth, randomHeight, randomRowPos, randomColPos, randomColor);
+            int[] rgba = [rand.nextInt(256)
+            ,            rand.nextInt(256)
+            ,canvas
+        ]
+        }
+        return rand;
+    }
+// Utility Methods
+
+    public static int[][] imgToTwoD(String inputFileOrLink) {
+        try {
+            BufferedImage image = null;
+            if (inputFileOrLink.substring(0, 4).toLowerCase().equals("http")) {
+                URL imageUrl = new URL(inputFileOrLink);
+                image = ImageIO.read(imageUrl);
+                if (image == null) {
+                    System.out.println("Failed to get image from provided URL.");
+                }
+            } else {
+                image = ImageIO.read(new File(inputFileOrLink));
+            }
+            int imgRows = image.getHeight();
+            int imgCols = image.getWidth();
+            int[][] pixelData = new int[imgRows][imgCols];
+            for (int i = 0; i < imgRows; i++) {
+                for (int j = 0; j < imgCols; j++) {
+                    pixelData[i][j] = image.getRGB(j, i);
+                }
+            }
+            return pixelData;
+        } catch (Exception e) {
+            System.out.println("Failed to load image: " + e.getLocalizedMessage());
+            return null;
         }
     }
-    return rand ;
-}
-// Utility Methods
-public static int[][] imgToTwoD(String inputFileOrLink) {
-		try {
-			BufferedImage image = null;
-			if (inputFileOrLink.substring(0, 4).toLowerCase().equals("http")) {
-				URL imageUrl = new URL(inputFileOrLink);
-				image = ImageIO.read(imageUrl);
-				if (image == null) {
-					System.out.println("Failed to get image from provided URL.");
-				}
-			} else {
-				image = ImageIO.read(new File(inputFileOrLink));
-			}
-			int imgRows = image.getHeight();
-			int imgCols = image.getWidth();
-			int[][] pixelData = new int[imgRows][imgCols];
-			for (int i = 0; i < imgRows; i++) {
-				for (int j = 0; j < imgCols; j++) {
-					pixelData[i][j] = image.getRGB(j, i);
-				}
-			}
-			return pixelData;
-		} catch (Exception e) {
-			System.out.println("Failed to load image: " + e.getLocalizedMessage());
-			return null;
-		}
-	}
-	public static void twoDToImage(int[][] imgData, String fileName) {
-		try {
-			int imgRows = imgData.length;
-			int imgCols = imgData[0].length;
-			BufferedImage result = new BufferedImage(imgCols, imgRows, BufferedImage.TYPE_INT_RGB);
-			for (int i = 0; i < imgRows; i++) {
-				for (int j = 0; j < imgCols; j++) {
-					result.setRGB(j, i, imgData[i][j]);
-				}
-			}
-			File output = new File(fileName);
-			ImageIO.write(result, "jpg", output);
-		} catch (Exception e) {
-			System.out.println("Failed to save image: " + e.getLocalizedMessage());
-		}
-	}
-	public static int[] getRGBAFromPixel(int pixelColorValue) {
-		Color pixelColor = new Color(pixelColorValue);
-		return new int[] { pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue(), pixelColor.getAlpha() };
-	}
-	public static int getColorIntValFromRGBA(int[] colorData) {
-		if (colorData.length == 4) {
-			Color color = new Color(colorData[0], colorData[1], colorData[2], colorData[3]);
-			return color.getRGB();
-		} else {
-			System.out.println("Incorrect number of elements in RGBA array.");
-			return -1;
-		}
-	}
-	public static void viewImageData(int[][] imageTwoD) {
-		if (imageTwoD.length > 3 && imageTwoD[0].length > 3) {
-			int[][] rawPixels = new int[3][3];
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					rawPixels[i][j] = imageTwoD[i][j];
-				}
-			}
-			System.out.println("Raw pixel data from the top left corner.");
-			System.out.print(Arrays.deepToString(rawPixels).replace("],", "],\n") + "\n");
-			int[][][] rgbPixels = new int[3][3][4];
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					rgbPixels[i][j] = getRGBAFromPixel(imageTwoD[i][j]);
-				}
-			}
-			System.out.println();
-			System.out.println("Extracted RGBA pixel data from top the left corner.");
-			for (int[][] row : rgbPixels) {
-				System.out.print(Arrays.deepToString(row) + System.lineSeparator());
-			}
-		} else {
-			System.out.println("The image is not large enough to extract 9 pixels from the top left corner");
-		}
-	}
+
+    public static void twoDToImage(int[][] imgData, String fileName) {
+        try {
+            int imgRows = imgData.length;
+            int imgCols = imgData[0].length;
+            BufferedImage result = new BufferedImage(imgCols, imgRows, BufferedImage.TYPE_INT_RGB);
+            for (int i = 0; i < imgRows; i++) {
+                for (int j = 0; j < imgCols; j++) {
+                    result.setRGB(j, i, imgData[i][j]);
+                }
+            }
+            File output = new File(fileName);
+            ImageIO.write(result, "jpg", output);
+        } catch (Exception e) {
+            System.out.println("Failed to save image: " + e.getLocalizedMessage());
+        }
+    }
+
+    public static int[] getRGBAFromPixel(int pixelColorValue) {
+        Color pixelColor = new Color(pixelColorValue);
+        return new int[]{pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue(), pixelColor.getAlpha()};
+    }
+
+    public static int getColorIntValFromRGBA(int[] colorData) {
+        if (colorData.length == 4) {
+            Color color = new Color(colorData[0], colorData[1], colorData[2], colorData[3]);
+            return color.getRGB();
+        } else {
+            System.out.println("Incorrect number of elements in RGBA array.");
+            return -1;
+        }
+    }
+
+    public static void viewImageData(int[][] imageTwoD) {
+        if (imageTwoD.length > 3 && imageTwoD[0].length > 3) {
+            int[][] rawPixels = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    rawPixels[i][j] = imageTwoD[i][j];
+                }
+            }
+            System.out.println("Raw pixel data from the top left corner.");
+            System.out.print(Arrays.deepToString(rawPixels).replace("],", "],\n") + "\n");
+            int[][][] rgbPixels = new int[3][3][4];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    rgbPixels[i][j] = getRGBAFromPixel(imageTwoD[i][j]);
+                }
+            }
+            System.out.println();
+            System.out.println("Extracted RGBA pixel data from top the left corner.");
+            for (int[][] row : rgbPixels) {
+                System.out.print(Arrays.deepToString(row) + System.lineSeparator());
+            }
+        } else {
+            System.out.println("The image is not large enough to extract 9 pixels from the top left corner");
+        }
+    }
 }
